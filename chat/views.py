af7a9e7c723473bcf551503fac63af2027ae85b4
kuldeep6445 <kuldeepbhogal111@gmail.com>
2021-03-12 22:32:54 +0530

    another commit

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate,logout
+import pymongo ,dns 
     
+client = pymongo.MongoClient("mongodb+srv://KSB:kuldeep@cluster0.gfqd2.mongodb.net/test?retryWrites=true&w=majority")
+db = client['test']
+
+
 def login_user1(request):
     a = authenticate(request,username='kuldeep',password='kuldeep')
     login(request,a)
@@ -22,9 +27,17 @@ def check_login(request):
     else:
         return render(request,'chat/login.html')
 
+def get_last_srno():
+    col = db['chat_dual']
+
+def update_mssage_to_db(text1 , request):
+    col = db['chat_dual']
+    num = get_last_srno()
+    dic = {'srno':'1'}
+
 def chatroom(request):
     text1 = request.POST.get('sent_mssg','')
-    print(text1)
+    update_mssage_to_db(text1,request)
     a = request.user
     context = {"user":a.username}
     return render(request,'chat/room.html',context)
